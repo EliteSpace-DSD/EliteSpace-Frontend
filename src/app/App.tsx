b95f764e25d2fb2d36ef8fb96a4e59d1c15609bd
FELIPE SANCHEZ <creative@vmgroupe.com>
2025-02-24 19:48:24 -0500

    Setup Mock API Call

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
 import AppProvider from "./Provider";
 import AppRouter from "./Router";
+import PostRequestComponent from "./Services/PostRequestComponent";
 
 function App() {
   return (
     <AppProvider>
       <AppRouter />
+      <PostRequestComponent />
     </AppProvider>
   );
 }
