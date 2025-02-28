2ecc6d593c8536b87c411cc63bab5339d8de9f75
FELIPE SANCHEZ <felipesanchez10@protonmail.com>
2025-02-28 17:58:57 -0500

    fix(MockApiCallComponent) New branch to fix issues with old deleted branch

--- src/components/MockApiCallComponent.tsx
+++ src/components/MockApiCallComponent.tsx
@@ -1,43 +1,7 @@
 import { useState } from "react";
+import { mockApiCall } from "../utils/api";
 
-interface ApiResponse {
-  status: number;
-  json: () => Promise<{ message?: string; error?: string }>;
-}
-
-const mockApiCall = (): Promise<ApiResponse> => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      //Mirrors the response of a server
-      const success = Math.random() > 0.2;
-
-      const body = success
-        ? { message: "Success!" }
-        : { error: "Server Error" };
-
-      const options = {
-        status: success ? 200 : 500,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
-      const response = new Response(JSON.stringify(body), options);
-      if (success) {
-        resolve({
-          status: 200,
-          json: () => Promise.resolve({ message: "Success!" }),
-        });
-      } else {
-        reject({
-          status: 500,
-          json: () => Promise.resolve({ error: "Server Error" }),
-        });
-      }
-    }, 1000);
-  });
-};
-
-const PostRequestComponent: React.FC = () => {
+const MockApiCallComponent: React.FC = () => {
   const [data, setData] = useState<{ message?: string } | null>(null);
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
@@ -99,4 +63,4 @@ const PostRequestComponent: React.FC = () => {
   );
 };
 
-export default PostRequestComponent;
+export default MockApiCallComponent;
