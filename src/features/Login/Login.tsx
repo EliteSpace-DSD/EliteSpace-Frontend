c1661969cdb859777eb1a41cb5365e99fc7c06a8
Eddie Maceda <eddiemaceda11@gmail.com>
2025-03-04 18:49:56 -0700

    feat(login): configure the UI layout for the login page

---
+++ src/features/Login/Login.tsx
@@ -0,0 +1,75 @@
+import { Box, TextField, Stack, InputLabel, Link, Button } from "@mui/material";
+
+const Login = () => {
+  return (
+    <div className="login-page">
+      <Stack className="login-container">
+        <Stack
+          component="section"
+          className="login-header-container"
+        >
+          <div>Logo</div>
+          <h1>EliteSpace</h1>
+        </Stack>
+        <Stack className="login-form">
+          <Stack className="email-container">
+            <InputLabel
+              htmlFor="email"
+              className="login-label"
+            >
+              Email
+            </InputLabel>
+            <TextField
+              className="email-input"
+              size="small"
+              variant="outlined"
+            />
+          </Stack>
+          <Stack className="password-container">
+            <label
+              htmlFor="password"
+              className="login-label"
+            >
+              Password
+            </label>
+            <TextField
+              className="password-input"
+              type="password"
+              size="small"
+              variant="outlined"
+            />
+          </Stack>
+          <Box className="remember-login">
+            <input
+              type="checkbox"
+              id="remember-me"
+            />
+            <label htmlFor="remember-me">Remember my login</label>
+          </Box>
+          <Stack className="login-btns-container">
+            <Button
+              variant="contained"
+              className="sign-in-btn"
+            >
+              Sign in
+            </Button>
+            <Button
+              variant="contained"
+              className="register-btn"
+            >
+              Register
+            </Button>
+          </Stack>
+          <Link
+            href="#"
+            underline="none"
+          >
+            Forgot your password?
+          </Link>
+        </Stack>
+      </Stack>
+    </div>
+  );
+};
+
+export default Login;
