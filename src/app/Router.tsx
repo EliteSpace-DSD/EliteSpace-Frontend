8d73ba747d66b43250849c39b6adf15a0250e037
Micheline <MDoughlin@hotmail.com>
2025-03-06 20:44:15 -0500

    (feat) Layout and mock information added

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,16 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import { MockComponent } from "../features/MockFeature/MockComponent";
 import HomePage from "./routes/HomePage";
+import { SmartPackage } from "../features/SmartPackage/SmartPackage";
+
 function AppRouter() {
   return (
     <Router>
       <Routes>
         {/* <Route path="/mock" element={<MockComponent />}></Route> */}
-        //{" "}
-        <Route
-          path="/"
-          element={<HomePage />}
-        ></Route>
+        // <Route path="/" element={<HomePage />}></Route>
+        <Route path="/smartpackage" element={<SmartPackage />}></Route>
       </Routes>
     </Router>
   );
---
+++ src/features/SmartPackage/SmartPackage.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
+import "./SmartPackage.css";
+
+let mockInformation = [
+  { package: "Package #1", status: "Delivered 2/12/25", time: "2:45PM" },
+  { package: "Package #2", status: "Delivered 2/12/25", time: "4:16PM" },
+];
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+  ...theme.applyStyles("dark", {
+    backgroundColor: "#1A2027",
+  }),
+}));
+
+export const SmartPackage = () => {
+  return (
+    <div className="smartPackageContainer">
+      <Box sx={{ width: "30%" }}>
+        <div className="itemContainer">
+          <Stack spacing={2}>
+            {mockInformation.map((item, index) => (
+              <Item key={index}>
+                {item.package}
+                <br />
+                {item.status}
+                <br />
+                {item.time}
+              </Item>
+            ))}
+          </Stack>
+        </div>
+      </Box>
+    </div>
+  );
+};
