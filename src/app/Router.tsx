c14823d05d86015a462ee16c056feed63bf1eb4b
Eddie Maceda <eddiemaceda11@gmail.com>
2025-03-10 20:37:00 -0700

    chore: cleaned up the Router.tsx file and saved lint/prettier changes

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,40 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router";
-import { MockComponent } from "../features/MockFeature/MockComponent";
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
+import { MockComponent } from '../features/MockFeature/MockComponent';
 
-import TestApiCall from "../features/TestApiCall/TestApiCall";
+import TestApiCall from '../features/TestApiCall/TestApiCall';
 
-import SignupPage from "../features/auth/SignupPage";
-
-import HomePage from "./routes/HomePage";
-import GuestParking from "../features/GuestParking/GuestParking";
-import GuestParkingApproved from "../features/GuestParking/GuestParkingApproved";
-import Login from "../features/Login/Login";
+import SignupPage from '../features/auth/SignupPage';
+import HomePage from './routes/HomePage';
+import GuestParking from '../features/GuestParking/GuestParking';
+import GuestParkingApproved from '../features/GuestParking/GuestParkingApproved';
+import Login from '../features/Login/Login';
 
 function AppRouter() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MockComponent />}></Route>
-        <Route path="/api-call" element={<TestApiCall />}></Route>
-        {/* <Route path="/mock" element={<MockComponent />}></Route> */}
-        <Route
-          path="/"
-          element={<HomePage />}
-        ></Route>
-        <Route
-          path="/signup"
-          element={<SignupPage />}
-        />
-        <Route
-          path="/parking"
-          element={<GuestParking />}
-        ></Route>
-        <Route
-          path="/parking/approved"
-          element={<GuestParkingApproved />}
-          path="/login"
-          element={<Login />}
-        ></Route>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='/signup' element={<SignupPage />} />
+        <Route path='/parking' element={<GuestParking />}></Route>
+        <Route path='/parking/approved' element={<GuestParkingApproved />}></Route>
+        <Route path='/login' element={<Login />}></Route>
       </Routes>
     </Router>
   );
