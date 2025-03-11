28e3c70cf0538c6b0bade50541add2300e808a01
Eddie Maceda <eddiemaceda11@gmail.com>
2025-03-10 20:37:57 -0700

    chore: delete unused imports

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,7 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router';
-import { MockComponent } from '../features/MockFeature/MockComponent';
-
-import TestApiCall from '../features/TestApiCall/TestApiCall';
 
 import SignupPage from '../features/auth/SignupPage';
 import HomePage from './routes/HomePage';
