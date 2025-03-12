c2018780eb98c936d19962e2291e8807a5910718
Eddie Maceda <eddiemaceda11@gmail.com>
2025-03-12 01:30:40 -0700

    feat(login): add the loginApi to the redux store

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,14 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import mockSlice from "../features/MockFeature/mockSlice";
 import { userApi } from "../features/Services/userSlice";
+import { loginApi } from "../features/Login/api/loginApi";
 
 const store = configureStore({
   reducer: {
     mock: mockSlice,
     [userApi.reducerPath]: userApi.reducer,
+    [loginApi.reducerPath]: loginApi.reducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(userApi.middleware),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(userApi.middleware, loginApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
